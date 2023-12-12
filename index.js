2644831f8afdab9911be5a3df14f0dd1c7cd5140
Emmanuel Oyewole <oyewoleemmanuel300@gmail.com>
2023-12-12 17:20:39 +0100

    update

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ let title = document.getElementById("title");
 let namefield = document.getElementById("namefield");
 
 /* adding click funtion event listener*/
-signinBtn.onclick = fuction(){
+signinBtn.onclick = function(){
     /* Hiding the name field on click*/
-    namefield.style.maxHeight = "0"
+    namefield.style.maxHeight = "0";
+    title.innerHTML = "Sign in"; /*changing the titlte on click*/
+    signupBtn.classList.add("disable");
+    signinBtn.classList.remove("disable");
+}
+
+signupBtn.onclick = function(){
+    /* Hiding the name field on click*/
+    namefield.style.maxHeight = "60px";
+    title.innerHTML = "Sign up"; /*changing the titlte on click*/
+    signupBtn.classList.remove("disable");
+    signinBtn.classList.add("disable");
 }
